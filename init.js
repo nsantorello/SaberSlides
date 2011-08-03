c0f39184032c865f66542215dbe5f549ec309e33
Noah Santorello <nsantorello@gmail.com>
2011-08-03 01:09:22 -0400

    Have easing working with slide transitions.

--- init.js
+++ init.js
@@ -3,9 +3,22 @@ function resizeSlides()
 	$('.slide').css('height', $(window).height());
 }
 
+function createButtons()
+{
+	$('.button').click(
+		function(e)
+		{
+			$.scrollTo('.slide2', 1500, { "easing": "easeOutQuint" });
+		}
+	);
+}
+
 $(document).ready(
 	function()
 	{
+		// Create buttons from the "button" class.
+		createButtons();
+		
 		// Resize slides initially, then hook into resize event.
 		resizeSlides();
 		$(window).resize(function(evt)
